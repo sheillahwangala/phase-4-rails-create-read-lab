e9030d62b9a541b552b854b0aab4538ac4e66556
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-25 11:48:50 +0300

    completed assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,30 @@
+class PlantsController < ApplicationController
+    # Index route - GET /plants
+    def index
+        plants = Plant.all 
+        render json: plants
+    end
+
+    # Show route - GET /plants/:id
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: { error: "Plant not found" }, status: :not_found
+        end
+    end
+
+    # create route - POST /plants
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+end
